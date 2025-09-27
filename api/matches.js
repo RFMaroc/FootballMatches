fa50b0a5ce313d85bf7b7d807b3dd527d2737f5d
RFMaroc <HASSAN_SBAI@ICLOUD.COM>
2025-09-27 12:43:59 +0200

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     const data = await response.json();
 
     const filteredMatches = data.response.filter(match =>
-      allowedLeagues.some(league => league.id === match.league.id)
+      allowedLeagues.some(league => Number(league.id) === Number(match.league.id))
     );
 
     res.status(200).json(filteredMatches);
